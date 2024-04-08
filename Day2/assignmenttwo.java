65235515e034bd0ee4caa438b2bc59d2dc20d0fe
occohen <ori@adalon.com>
2024-04-08 16:05:34 -0700

    updates
    a

---
+++ Day2/assignmenttwo.java
@@ -0,0 +1,34 @@
+
+
+
+
+/* 
+Give at least two examples (in real life and in programming) and a short description for:
+Stacks: LIFO = Last in first out. 
+This means that anything entered into the stack will go to the end of the list, 
+and anything removed (popped) will go out from the end of the list.
+Programming example: Version history. For example, in a google document. If I make a change, then it gets added to the end of the stack. If I wanted to "undo," I would pop it from the end of the stack. This means that the most recent action will be undone every time, and new actions will be added to the end.
+Real life example: Sorting a pile of items. When new items need to be sorted, they are added to the end of the stack. When I take something out of the stack to sort it, I remove it from the top of the list (since taking it from the bottom of the pile would be impractical). The limitation to this is that items can be forgotten at the bottom of the pile as new items are added to the top.
+
+Queues: FIFO = First in first out.
+This means that anything entered into the stack will go to the end of the list (the same as in Stacks). 
+This is called enqueue.
+Queues also mean that anything dequeued from the queue will be removed from the beginning of the list.
+Programming example: Printer queues. When you print something, your computer will add it to the end of the queue of things that the printer needs to print. The printer will take items off the queue as it prints them, starting at the bottom and working its way to the top.
+Real life example: A line at an ice cream shop. New customers enter the end of the line, and the store serves the people from the front of the line (thereby removing them from the queue).
+
+Deques: Double ended queue.
+This means that things can be removed or added to either side of the list (front or back).
+Programming example: Search history. As more entries are added to the end (more searches), the deque is expanded to the right (or the front). At a certain point, the capacity for total storage is fulfilled, and the searches from the beginning need to be removed. Using a deque would allow users to also remove searches from the end of the list more efficiently.
+Real life example: 
+
+*/
+
+
+package Day2;
+
+class assignmenttwo{
+    public static void main(String[] args) {
+        
+    }
+}
