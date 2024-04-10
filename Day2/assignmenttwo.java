50877c673bc25ee4e87b40796a4ec3b584b28622
occohen <ori@adalon.com>
2024-04-09 23:23:02 -0700

    done

--- Day2/assignmenttwo.java
+++ Day2/assignmenttwo.java
@@ -28,11 +28,67 @@ package Day2;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 class assignmenttwo{
-
+    //binary search (problem 4)
+    public static int binarySearch(int[] sortedArray, int startIndex, int endIndex, int target){
+        //element not found
+        if(startIndex > endIndex)
+        {
+            System.out.println("Element not found");
+            return -1;
+        }
+        //get middle index
+        int middleIndex = startIndex + (endIndex - startIndex) / 2;
+        //target found
+        if(sortedArray[middleIndex] == target){
+            return middleIndex;
+        }
+        //target is to the left of mid index
+        else if(sortedArray[middleIndex] > target){
+            return binarySearch(sortedArray, startIndex, middleIndex - 1, target);
+        //target is to the right of mid index
+        }else if(sortedArray[middleIndex] < target){
+            return binarySearch(sortedArray, middleIndex + 1, endIndex, target);
+        }else{
+            //should never reach this case.
+            System.out.println("Reached end somehow?");
+            return -1;
+        }
+    }
+    //class for Deque
     public static class Deque{
+        //instance var
         public int[] dequeArray;
+        //constructor
         public Deque(int[] dequeArray){
             this.dequeArray = dequeArray;
+        }
+        public int getCount(){
+            return this.dequeArray.length;
+        }
+        public int peekLeft(){
+            return this.dequeArray[0];
+        }
+        public int peekRight(){
+            return this.dequeArray[this.dequeArray.length - 1];
+        }
+        
+        public int popLeft(){
+            int poppedElement = this.dequeArray[0];
+            int[] newArray = new int[this.dequeArray.length - 1];
+            for(int i = 1; i < this.dequeArray.length; i++){
+                newArray[i - 1] = this.dequeArray[i];
+            }
+            this.dequeArray = newArray;
+            return poppedElement;
+        }
+        public int popRight(){
+            int poppedElement = this.dequeArray[this.dequeArray.length - 1];
+            int[] newArray = new int[this.dequeArray.length - 1];
+            for(int i = 0; i < newArray.length; i++){
+                newArray[i] = this.dequeArray[i];
+            }
+            this.dequeArray = newArray;
+            return poppedElement;
         }
         public void appendRight(int insertValue){
             int[] newArray = new int[this.dequeArray.length + 1];
@@ -53,7 +109,7 @@ class assignmenttwo{
         public void printDeque(){
             System.out.println("new array is:");
             for(int i = 0; i < this.dequeArray.length; i++){
-                System.out.print(this.dequeArray[i]);
+                System.out.print(this.dequeArray[i] + ",");
             }
             System.out.println("");
         }
@@ -63,10 +119,41 @@ class assignmenttwo{
     }
 
 
-
-
-
+    //Check Balanced Braces
+    public static Boolean checkBalanced(String input){
+        //edgecase, string empty
+        if(input.length() == 0)return true;
+        //create deque
+        ArrayDeque<Character> inputArray = new ArrayDeque<Character>();
+        //loop through the input string
+        for(int i = 0; i < input.length();i++){
+            //add opening braces to stack
+            if(Character.toString(input.charAt(i)).equals("(") || Character.toString(input.charAt(i)).equals("{") || Character.toString(input.charAt(i)).equals("[")){
+                inputArray.offerLast((input.charAt(i)));
+            }
+            //check closing braces against most recent opening brace in the stack
+            else if(Character.toString(input.charAt(i)).equals(")") || Character.toString(input.charAt(i)).equals("}") || Character.toString(input.charAt(i)).equals("]")){
+                if(inputArray.isEmpty())return false;
+                String mostRecent = Character.toString(inputArray.removeLast());
+                String current = Character.toString(input.charAt(i));
+                //mismatches
+                if(current.equals(")") && !mostRecent.equals( "(")){
+                        return false;
+                }
+                else if(current.equals("]") && !mostRecent.equals( "[")){
+                        return false;
+                }
+                else if(current.equals("}") && !mostRecent.equals( "{")){
+                        return false;
+                }
+            }
+        }
+        //if array is empty, all the braces found a match = should return true
+        return (inputArray.isEmpty());
+    }
+    //driver method
     public static void main(String[] args) {
+        //loop through test cases
         ArrayList<String> tests = new ArrayList<String>();
         tests.add("[ ]");
         tests.add("{}{}[]()");
@@ -77,9 +164,10 @@ class assignmenttwo{
         tests.add("[{)}]");
         tests.add("(()[()({})]]])");
         for(int i = 0; i < tests.size(); i++){
-            //System.out.println("test case" + i + checkBalanced(tests.get(i)));
+            System.out.println("test case" + i + checkBalanced(tests.get(i)));
         }
-
+        
+        //test deque
         int[] arrayOne = new int[5];
         Deque dequeArray = new Deque(arrayOne);
         dequeArray.setIndex(0,1);
@@ -94,42 +182,28 @@ class assignmenttwo{
         dequeArray.appendRight(5);
         dequeArray.printDeque();
 
-    } 
-    //Check Balanced Braces
-    public static Boolean checkBalanced(String input){
-        //edgecase, string empty
-        if(input.length() == 0)return true;
-        
-        ArrayDeque<Character> inputArray = new ArrayDeque<Character>();
-        for(int i = 0; i < input.length();i++){
-            if(Character.toString(input.charAt(i)).equals("(") || Character.toString(input.charAt(i)).equals("{") || Character.toString(input.charAt(i)).equals("[")){
-                inputArray.offerLast((input.charAt(i)));
-            }
-            else if(Character.toString(input.charAt(i)).equals(")") || Character.toString(input.charAt(i)).equals("}") || Character.toString(input.charAt(i)).equals("]")){
-                if(inputArray.isEmpty())return false;
-                //System.out.println(inputArray);
-                //System.out.println(inputArray.size());
-                String mostRecent = Character.toString(inputArray.removeLast());
-                String current = Character.toString(input.charAt(i));
-                //System.out.println(mostRecent);
-                //System.out.println(current);
-                if(current.equals(")") && !mostRecent.equals( "(")){
-                        return false;
-                }
-                else if(current.equals("]") && !mostRecent.equals( "[")){
-                        return false;
-                }
-                else if(current.equals("}") && !mostRecent.equals( "{")){
-                        return false;
-                }
-                //System.out.println(inputArray);
-            }
-        }
-        //System.out.println(inputArray);
-        return (inputArray.isEmpty());
-        
-        
-        
+        System.out.println("Pop right");
+        System.out.println(dequeArray.popRight());
+        dequeArray.printDeque();
 
-    }
+        System.out.println("Pop left");
+        System.out.println(dequeArray.popLeft());
+        dequeArray.printDeque();
+
+        System.out.println("Peek Left" + dequeArray.peekLeft());
+
+        System.out.println("Peek Right" + dequeArray.peekRight());
+
+        System.out.println("Current count is:" + dequeArray.getCount());
+
+        //Test binary search
+        int[] sortedArray = new int[30];
+        for(int i = 0; i < sortedArray.length; i++){
+            sortedArray[i] = (i * 3) - 1;
+        }
+        Deque sortedDeque = new Deque(sortedArray);
+        sortedDeque.printDeque();
+        //give some random target, see if it is in
+        System.out.println(binarySearch(sortedArray, 0, sortedArray.length - 1, 44));
+    } 
 }
